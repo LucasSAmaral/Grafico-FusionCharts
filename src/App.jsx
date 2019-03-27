34b083cd37c506c15052f3ffc091a4302c600784
Lucas Santos do Amaral <lucas.amaral@MJV.net>
2019-03-27 10:55:48 -0300

    Componentizando os graficos

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,19 @@
 import React, { Component } from 'react';
-import ReactFC from 'react-fusioncharts';
-import FusionCharts from 'fusioncharts';
-import Column2D from 'fusioncharts/fusioncharts.charts';
-import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+import Column2D from './Column2D';
+import Column3D from './Column3D';
 import './App.css';
 
-
-ReactFC.fcRoot(FusionCharts, Column2D, FusionTheme);
-
-const chartConfigs = {
-  type: 'column2d',// The chart type
-  width: '700', // Width of the chart
-  height: '400', // Height of the chart
-  dataFormat: 'json', // Data type
-  dataSource: {
-      // Chart Configuration
-      "chart": {
-          "animation": 1,
-          "animationDuration": 1,
-          "setAdaptiveYMin": 1,
-          "baseFontColor": "#FFFFFF",
-          "toolTipBgColor": "#5a5a5a",
-          "baseFontSize": 14,
-          "paletteColors": "#FF0000,#00FF00,#0000FF,#FF00FF,#00FFFF,#FFFF00,#FFF06F,#000000",
-          "caption": "Países com maiores reservas de óleo [2017-18]",
-          "subCaption": "In MMbbl = One Million barrels",
-          "xAxisName": "País",
-          "yAxisName": "Reservas (MMbbl)",
-          "canvasBGColor":"#282c34",
-          "BGColor":"#282c34",
-          "numberSuffix": "Mil",
-          "theme": "fusion",
-      },
-      // Chart Data
-      "data": [{
-          "label": "Venezuela",
-          "value": "290"
-      }, {
-          "label": "Saudi",
-          "value": "260"
-      }, {
-          "label": "Canadá",
-          "value": "180"
-      }, {
-          "label": "Irã",
-          "value": "140"
-      }, {
-          "label": "Rússia",
-          "value": "115"
-      }, {
-          "label": "UAE",
-          "value": "100"
-      }, {
-          "label": "EUA",
-          "value": "30"
-      }, {
-          "label": "China",
-          "value": "30"
-      }]
-  }
-};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <ReactFC {...chartConfigs} />
+        <div className="container__charts">
+            <Column2D />
+        </div>
+        <div className="container__charts">
+            <Column3D />
+        </div>
       </div>
     );
   }
---
+++ src/Column2D.jsx
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import ReactFC from 'react-fusioncharts';
+import FusionCharts from 'fusioncharts';
+import Column2D from 'fusioncharts/fusioncharts.charts';
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+
+ReactFC.fcRoot(FusionCharts, Column2D, FusionTheme);
+
+const chartConfigs = {
+    type: 'column2d',// The chart type
+    width: '100%', // Width of the chart
+    height: '400', // Height of the chart
+    dataFormat: 'json', // Data type
+    dataSource: {
+        // Chart Configuration
+        "chart": {
+            "animation": 1,
+            "animationDuration": 1,
+            "setAdaptiveYMin": 1,
+            "baseFontColor": "#FFFFFF",
+            "toolTipBgColor": "#5a5a5a",
+            "baseFontSize": 14,
+            "paletteColors": "#FF0000,#00FF00,#0000FF,#FF00FF,#00FFFF,#FFFF00,#FFF06F,#000000",
+            "caption": "Países com maiores reservas de óleo [2017-18]",
+            "subCaption": "In MMbbl = One Million barrels",
+            "xAxisName": "País",
+            "yAxisName": "Reservas (MMbbl)",
+            "BGColor":"#282c34",
+            "numberSuffix": "Mil",
+            "theme": "fusion",
+        },
+        // Chart Data
+        "data": [{
+            "label": "Venezuela",
+            "value": "290"
+        }, {
+            "label": "Saudi",
+            "value": "260"
+        }, {
+            "label": "Canadá",
+            "value": "180"
+        }, {
+            "label": "Irã",
+            "value": "140"
+        }, {
+            "label": "Rússia",
+            "value": "115"
+        }, {
+            "label": "UAE",
+            "value": "100"
+        }, {
+            "label": "EUA",
+            "value": "30"
+        }, {
+            "label": "China",
+            "value": "30"
+        }]
+    }
+  };  
+
+class Col2D extends Component {
+    render(){
+        return(<ReactFC {...chartConfigs} />)
+    }
+}
+
+export default Col2D
